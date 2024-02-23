664b1b574a68a2a7df18612c221cba291a311611
Isabella Zarosa <isabellazarosa@gmail.com>
2024-02-23 17:51:26 -0300

    Update Agencia.py

--- Agencia.py
+++ Agencia.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Agencia:
 
     def __init__(self, telefone, cnpj, numero):
@@ -27,33 +30,66 @@ class Agencia:
 class AgenciaVirtual(Agencia):  # Agencia virtual é subclasse de agencia
     def __init__(self, site, telefone, cnpj):
         self.site = site
-        super().__init__(telefone, cnpj, 1000) # Roda o init da classe principal
+        super().__init__(telefone, cnpj, 1000)  # Roda o init da classe principal
         self.caixa = 1000000
+        self.caixa_paypal = 0
+
+    def depositar_paypal(self, valor):
+        self.caixa -= valor  # Não restringi os limites dos caixas (em todos estão assim quase)
+        self.caixa_paypal += valor
+
+    def sacar_paypal(self, valor):
+        self.caixa_paypal -= valor
+        self.caixa += valor
 
 
 class AgenciaComum(Agencia):
-    pass
+    def __init__(self, telefone, cnpj):
+        super().__init__(telefone, cnpj, numero=randint(1001, 9999))  # Roda o init da classe principal
+        self.caixa = 1000000
 
 
 class AgenciaPremium(Agencia):
-    pass
+    def __init__(self, telefone, cnpj):
+        super().__init__(telefone, cnpj, numero=randint(1001, 9999))  # Roda o init da classe principal
+        self.caixa = 10000000
+
+    def adicionar_cliente(self, nome, cpf, patrimonio):
+        if patrimonio > 1000000:
+            super().adicionar_cliente(nome, cpf, patrimonio)
+        else:
+            print('O cliente não tem o patrimônio mínimo necessário para entrar na Agência Premium')
 
 
-agencia1 = Agencia(33444555, 437463463468374, 4554)
+if __name__ == '__main__':
+    agencia1 = Agencia(33444555, 437463463468374, 4554)
 
-agencia1.caixa = 1000000
+    agencia1.caixa = 1000000
 
-agencia1.verificar_caixa()
+    agencia1.verificar_caixa()
 
-agencia1.emprestar_dinheiro(1500, 827377389999, 0.03)
+    agencia1.emprestar_dinheiro(1500, 827377389999, 0.03)
 
-agencia1.adicionar_cliente('Isa', 11111111111, 10000)
+    agencia1.adicionar_cliente('Isa', 11111111111, 10000)
 
-print(agencia1.clientes)
+    print(agencia1.clientes)
 
-agencia_virtual = AgenciaVirtual('ygygg', 222, 33333333333)
+    agencia_virtual = AgenciaVirtual('ygygg', 222, 33333333333)
 
-agencia_virtual.caixa = 15000
-agencia_virtual.verificar_caixa()  # Herdou os métodos e atributos da superclasse
+    agencia_virtual.caixa = 15000
+    agencia_virtual.verificar_caixa()  # Herdou os métodos e atributos da superclasse
 
-print(agencia_virtual.clientes)
+    print(agencia_virtual.site)
+
+    agencia_comum = AgenciaComum(111111111, 2222222)
+    agencia_comum.verificar_caixa()
+
+    agencia_premium = AgenciaPremium(33332333, 888888)
+    agencia_premium.verificar_caixa()
+
+    agencia_virtual.depositar_paypal(20000)
+    print(agencia_virtual.caixa)
+    print(agencia_virtual.caixa_paypal)
+
+    agencia_premium.adicionar_cliente('ux', 3920, 939208765443)
+    print(agencia_premium.clientes)
